7e579ad511265c456551234114514d50aac294b7
JacobiClark <jacobcs@zoho.com>
2021-09-22 02:27:58 -0700

    completed post method to Flask backend

--- api/app.py
+++ api/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import request
 from flask_cors import CORS #comment this on deployment
 import pandas as pd
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
 
-@app.route("/milestones")
-def hello_world():
-    df = pd.read_excel ('milestones.xlsx').to_json()
-    return df
+@app.route("/milestones", methods = ['GET', 'POST'])
+def upload_milestones():
+    if request.method == 'GET':
+        df = pd.read_excel ('milestones.xlsx').to_json()
+        return df
+    if request.method == 'POST':
+        content = request.json
+        print(content)
+        return content
+
--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import { useState, useEffect } from "react";
 import { Box, Button, Flex } from "@chakra-ui/react";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [milestones, setMilestones] = useState([]);
+  const [selectedMilestones, setSelectedMilestones] = useState([]);
+  const [selectedCompletionDates, setSelectedCompletionDates] = useState({});
+
+  const [milestoneData, setMilestoneData] = useState([]);
+  const [completionDateData, setcompletionDateData] = useState([]);
+
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+
+  const handleMilestoneButtonClick = (e) => {
+    e.preventDefault();
+    let mapped = milestoneData.map((milestone) => {
+      return milestone.milestone == e.target.value
+        ? { ...milestone, isSelected: !milestone.isSelected }
+        : { ...milestone };
+    });
+    setMilestoneData(mapped);
+    console.log(e.target.value);
+  };
+
+  const handleCompletionDateButtonClick = (e) => {
+    e.preventDefault();
+    setSelectedCompletionDates({ selectedCompletionDate: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log("post initiated");
+    fetch("http://localhost:5000/milestones", {
+      method: "post",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ a: 7, str: "Some string: &=&" }),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,7 +51,19 @@ function App() {
       try {
         const response = await fetch("http://localhost:5000/milestones");
         const responseJson = await response.json();
-        setMilestones(responseJson);
+        let milestoneData = Object.values(responseJson.Milestone);
+        milestoneData = milestoneData.map((milestone) => {
+          let milestoneObj = {
+            milestone: milestone,
+            isSelected: false,
+          };
+          return milestoneObj;
+        });
+        setMilestoneData(milestoneData);
+        const completionDateData = Object.values(
+          responseJson["Completion date"]
+        );
+        setcompletionDateData(completionDateData);
       } catch (error) {
         setIsError(true);
       }
@@ -36,11 +82,33 @@ function App() {
   }
   return (
     <Box>
-      <Box>{JSON.stringify(milestones, null, 2)}</Box>
-      <Flex direction="column">
-        {milestones.Milestone["0"]}
-        {Object.keys(milestones.Milestone)}
+      <Flex>
+        {milestoneData.map((milestone) => (
+          <Button
+            value={milestone.milestone}
+            onClick={handleMilestoneButtonClick}
+            colorScheme={milestone.isSelected === true ? "teal" : "facebook"}
+          >
+            {milestone.milestone}
+          </Button>
+        ))}
       </Flex>
+      <Flex>
+        {completionDateData.map((completionDate) => (
+          <Button
+            value={completionDate}
+            onClick={handleCompletionDateButtonClick}
+            colorScheme={
+              selectedCompletionDates.selectedCompletionDate === completionDate
+                ? "teal"
+                : "facebook"
+            }
+          >
+            {completionDate}
+          </Button>
+        ))}
+      </Flex>
+      <Button onclick={handleSubmit}>submit</Button>
     </Box>
   );
 }
