a8c7ad0d391cb71ed3cfa369a9d51144cfe8bf1a
JacobiClark <jacobcs@zoho.com>
2021-09-26 07:38:54 -0700

    added custon file selection && implemented child process spawn

--- api/app.py
+++ api/app.py
@@ -7,25 +7,24 @@ import platform
 import subprocess
 
 app = Flask(__name__)
-CORS(app) #comment this on deployment
+CORS(app, supports_credentials=True) #comment this on deployment
+
+
 
 @app.route("/milestones", methods = ['GET', 'POST'])
-def upload_milestones():
-    if request.method == 'GET':
-        if platform.system() == "Windows":
-            subprocess.Popen(r'explorer /select,' + str('C:/Users/'))
-        elif platform.system() == "Darwin":
-            subprocess.Popen(["open", 'C:/Users/'])
-        else:
-            subprocess.Popen(["xdg-open", 'C:/Users/'])
-        milestones = pd.read_excel('milestones.xlsx').to_json()
-        return milestones
-    
+def upload_milestones():    
     if request.method == 'POST':
         selected_milestones = request.json
+        print(request)
         pd.DataFrame.from_dict(selected_milestones).to_excel("my_selected_milestones.xlsx", index=False)
         return selected_milestones
 
+@app.route("/retrievemilestones", methods = ['GET', 'POST'])
+def upload_milestoness():    
+    if request.method == 'POST':
+        milestoneFile = request.json
+        milestones = pd.read_excel(milestoneFile["filePath"]).to_json()
+        return milestones
+
 if __name__ == "__main__":
     app.run()
-
