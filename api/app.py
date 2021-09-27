710ef0522b03b79558a6f30b559407525f603f42
JacobiClark <jacobcs@zoho.com>
2021-09-27 10:08:39 -0700

    changed python function name

--- api/app.py
+++ api/app.py
@@ -13,18 +13,14 @@ CORS(app, supports_credentials=True) #comment this on deployment
 
 @app.route("/milestones", methods = ['GET', 'POST'])
 
-def download_selected_milestones():
-    if request.method == 'GET':
-        milestones = pd.read_excel('milestones.xlsx').to_json()
-        return milestones
-        
+def download_selected_milestones():        
     if request.method == 'POST':
         selected_milestones = request.json
         pd.DataFrame.from_dict(selected_milestones).to_excel("my_selected_milestones.xlsx", index=False)
         return selected_milestones
 
 @app.route("/retrievemilestones", methods = ['GET', 'POST'])
-def retrieve_milestoness():    
+def retrieve_milestones():    
     if request.method == 'POST':
         milestoneFile = request.json
         milestones = pd.read_excel(milestoneFile["filePath"]).to_json()
